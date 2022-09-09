a15c9688bec40848fba42df023de470f93e1a198
Ricardo Gastón Plazas <rgplazas1990@gmail.com>
2022-09-09 13:29:44 -0300

    Fix(cambios)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,30 @@
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Contexto from "../context/Contexto";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import iconoCabecera from "../assets/statics/loading.gif";
 export default function Header() {
+    const { loginWithGoogle } = useContext(Contexto);
+
+    const navigate = useNavigate();
+
+    const handleLogin = async () => {
+        try {
+            await loginWithGoogle();
+            navigate("/");
+        } catch (error) {
+            console.log(error.message);
+        }
+
+    }
+
     return (
         <>
             <Navbar collapseOnSelect expand="xl" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="/"><img alt="" src={iconoCabecera} width="30" height="30" className="d-inline-block align-top"/>{' '}Retos de Programación</Navbar.Brand>
+                    <Navbar.Brand href="/"><img alt="" src={iconoCabecera} width="30" height="30" className="d-inline-block align-top" />{' '}Retos de Programación</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
@@ -18,7 +35,7 @@ export default function Header() {
                             <Nav.Link href="/semanales2022">Ejercicios Semanales '22</Nav.Link>
                             <Nav.Link href="/mensuales2022">Apps Mensuales '22</Nav.Link>
                             <Nav.Link href="https://github.com/rgplazas/desafio-semanal-2022-POO">Proyecto en GitHub</Nav.Link>
-                            <button className="DCButton_DCButton__2G827 DCButton_DCButton__outline__mECyy undefined" aria-label="Login with github"><div className="DCButton_DCButton_icon__l9p8v DCButton_DCButton_icon__marginRight__9b00y"></div><div className="DCButton_DCButton_wrapper__1uexv">Login Github</div></button>
+                            <button id="login" name="login" onClick={handleLogin} className="DCButton_DCButton__2G827 DCButton_DCButton__outline__mECyy undefined" aria-label="Login with github"><div className="DCButton_DCButton_icon__l9p8v DCButton_DCButton_icon__marginRight__9b00y"></div><div className="DCButton_DCButton_wrapper__1uexv">Login Github</div></button>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
--- src/context/UsarContexto.js
+++ src/context/UsarContexto.js
@@ -1,58 +1,64 @@
 import Contexto from "./Contexto";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // importacion de firebase
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, onValue, query, orderByChild} from "firebase/database";
+import { getDatabase, ref, onValue, query, orderByChild } from "firebase/database";
+import { getAuth , GoogleAuthProvider, signInWithPopup, sendPasswordResetEmail } from "firebase/auth";
 
 export default function UsarContexto(props) {
 
     // firebase configurations
-const firebaseConfig = {
-    apiKey: "AIzaSyDHmSEJvkUmCQb6SdO2OBzS1vb4bD68w2w",
-    authDomain: "desafio-semanal-2022-poo.firebaseapp.com",
-    databaseURL: "https://desafio-semanal-2022-poo-default-rtdb.firebaseio.com",
-    projectId: "desafio-semanal-2022-poo",
-    storageBucket: "desafio-semanal-2022-poo.appspot.com",
-    messagingSenderId: "367268496132",
-    appId: "1:367268496132:web:7dd147a16490f12d90c651",
-    measurementId: "G-VHV17QR13E"
-};
+    const firebaseConfig = {
+        apiKey: "AIzaSyDHmSEJvkUmCQb6SdO2OBzS1vb4bD68w2w",
+        authDomain: "desafio-semanal-2022-poo.firebaseapp.com",
+        databaseURL: "https://desafio-semanal-2022-poo-default-rtdb.firebaseio.com",
+        projectId: "desafio-semanal-2022-poo",
+        storageBucket: "desafio-semanal-2022-poo.appspot.com",
+        messagingSenderId: "367268496132",
+        appId: "1:367268496132:web:7dd147a16490f12d90c651",
+        measurementId: "G-VHV17QR13E"
+    };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const db = getDatabase(app);
+    const auth = getAuth(app);
 
-//referencias firebase
-const refRetosSemanal = ref(db, "retosSemanal/");
-const refRetosMensual = ref(db, "retosMensual/");
+    //referencias firebase
+    const refRetosSemanal = ref(db, "retosSemanal/");
+    const refRetosMensual = ref(db, "retosMensual/");
 
-//query firebase
-const queryRetosSemanal = query(refRetosSemanal, orderByChild("order/"));
-const queryRetosMensual = query(refRetosMensual, orderByChild("order/"));
-//const queryLimitToFirstRetos = query(refRetosSemanal, limitToFirst(2)); //trae los primeros dos semanas de retosSemanal
-//Estados de los useState
-const [estadoSemanal, setEstadoSemanal] = useState([]);
-const [estadoMensual, setEstadoMensual] = useState([]);
+    //query firebase
+    const queryRetosSemanal = query(refRetosSemanal, orderByChild("order/")); // ordenar por la clave orden todos los elementos semanal
+    const queryRetosMensual = query(refRetosMensual, orderByChild("order/")); // ordenar por la clave orden todos los elementos mensual
+    //const queryRetosSemanal = query(refRetosSemanal, limitToFirst(3)); //trae los primeros tres retos de retosSemanal
+    //const queryRetosMensual = query(refRetosMensual, limitToFirst(3)); //trae los primeros tres retos de retosMensual
+    //const queryRetosSemanal = query(refRetosSemanal, limitToLast(3)); //trae los ultimos tres retos de retosSemanal
+    //const queryRetosMensual = query(refRetosMensual, limitToLast(3)); //trae los ultimos tres retos de retosMensual
+    //Estados de los useState
+    const [estadoSemanal, setEstadoSemanal] = useState([]);
+    const [estadoMensual, setEstadoMensual] = useState([]);
 
-const { children } = props;
+    const { children } = props;
 
-/*const estadoInicial = {
-    retosSemanal: []
-}*/
+    const listameRetosSemanal = async () => {
+        onValue(queryRetosSemanal, (snap) => {
+            let data = snap.val();
+            setEstadoSemanal(data);
+        })
+    }
 
-const listameRetosSemanal = async () => {
-    onValue(queryRetosSemanal, (snap) => {
-        let data = snap.val();
-        setEstadoSemanal(data);
-    })
-}
+    const listameRetosMensual = async () => {
+        onValue(queryRetosMensual, (snap) => {
+            let data = snap.val();
+            setEstadoMensual(data);
+        })
+    }
 
-const listameRetosMensual = async () => {
-    onValue(queryRetosMensual, (snap) => {
-        let data = snap.val();
-        setEstadoMensual(data);
-    })
-}
+    const loginWithGoogle = () => {
+        const googleProvider = new GoogleAuthProvider();
+        return signInWithPopup(auth, googleProvider);
+    };
 
     return (
         <>
@@ -60,7 +66,8 @@ const listameRetosMensual = async () => {
                 retosSemanal: estadoSemanal,
                 retosMensual: estadoMensual,
                 listameRetosSemanal,
-                listameRetosMensual
+                listameRetosMensual,
+                loginWithGoogle
             }}>
                 {children}
             </Contexto.Provider>
